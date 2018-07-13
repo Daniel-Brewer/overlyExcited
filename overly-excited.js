3bd18b030e1adb87be00ed41f925986c070a05af
Daniel Brewer <danielbrewer2014@comcast.net>
2018-07-13 09:47:08 -0500

    solved overlyExcited

--- overly-excited.js
+++ overly-excited.js
@@ -1,24 +1,25 @@
 // Create an array that contains the words in the sentence
 let sentence = ["The","walrus","danced","through","the","trees","in","the","light","of","the","moon"];
+var symbol = "?";
 
-/*
-    The addExcitement function should be an impure function, and accept
-    the array as the sole argument. It should iterate over the array
-    and output the words to the browser console.
-*/
-function addExcitement (theWordArray) {
-
-    // Each time the for loop executes, you're going to add one more word to this string
+function addExcitement (theWordArray, symbol) {
     let buildMeUp = ""
-
+    var j = 1;
     for (let i = 0; i < theWordArray.length; i++) {
-        // Concatenate the new word onto buildMeUp
+        if ((i + 1) % 3 === 0) {
+        
+            buildMeUp = buildMeUp + " " + theWordArray[i] + symbol.repeat(j);
+            console.log(buildMeUp);
+            j = j + 1;
+        }
+        else {
         buildMeUp = buildMeUp + " " + theWordArray[i];
         console.log(buildMeUp);
-        // Print buildMeUp to the console
+        }
+    
     }
 
 }
 
 // Invoke the function and pass in the array
-addExcitement(sentence);
+addExcitement(sentence, "?");
